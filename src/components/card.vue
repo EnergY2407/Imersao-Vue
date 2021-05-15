ccad77ad514da3c42300f3d960ed23f0bd07e54f
EnergY2407 <53410403+EnergY2407@users.noreply.github.com>
2021-05-15 11:41:30 -0300

    Metade aula 2

---
+++ src/components/card.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="card" >
+  <div class="card-title">Titulo</div>
+  <div>Corpo</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+<style scoped>
+.card{
+border:1px solid black;
+margin:5px;
+padding:5px;
+width:200px;
+height:200px
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="home">
+  componente home.vue
+<div style="display:flex">
+  <div class="card" >
+  <div style="border-bottom: 1px solid black">Titulo</div>
+  <div>Corpo</div>
   </div>
+     <div class="card" >
+  <div style="border-bottom: 1px solid black">Titulo</div>
+  <div>Corpo</div>
+  </div>
+     <div class="card" >
+  <div style="border-bottom: 1px solid black">Titulo</div>
+  <div>Corpo</div>
+  </div>
+</div>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
   components: {
-    HelloWorld,
   },
 });
 </script>
+<style scoped>
+.card{
+border:1px solid black;
+margin:5px;
+padding:5px;
+width:200px;
+height:200px
+}
+
+</style>
