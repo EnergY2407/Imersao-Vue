533d4a00258756e11b7b27dd4a02408f1d186f02
EnergY2407 <53410403+EnergY2407@users.noreply.github.com>
2021-05-16 15:15:47 -0300

    Outra metade da aula 2
    tem erros diversos arrumar depois

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,21 +1,48 @@
 <template>
-<div class="card" >
-  <div class="card-title">Titulo</div>
-  <div>Corpo</div>
+  <div class="card">
+    <div>{{a}}</div>
+    <button @click="clickHandler">++</button>
+    <div class="card-title">Titulo
+      {{ title }}
+    </div>
+    <div>{{ body }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    title: { type: String, default: '' },
+    body: { type: String, default: '' },
+  },
+
+  // composition API
+  setup() {
+    const a = ref(0);
+
+    const clickHandler = (e: MouseEvent) => {
+      console.log(a.value);
+      a.value += 1;
+    };
+    return {
+      a,
+      clickHandler,
+    };
+  },
+});
 </script>
 <style scoped>
-.card{
-border:1px solid black;
-margin:5px;
-padding:5px;
-width:200px;
-height:200px
+.card {
+  border: 1px solid black;
+  margin: 5px;
+  padding: 5px;
+  width: 200px;
+  height: 200px;
+}
+
+.card-title {
+  border-bottom: 1px solid black;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,23 @@
 <template>
-<div class="home">
-  componente home.vue
-<div style="display:flex">
-  <div class="card" >
-  <div style="border-bottom: 1px solid black">Titulo</div>
-  <div>Corpo</div>
+  <div class="home">
+    componente home.vue
+    <div style="display: flex; flex-wrap: wrap">
+<card title="Titulo 1" body="Corpo"/>
+<card title="Novo titulo" body="Novo card"/>
   </div>
-     <div class="card" >
-  <div style="border-bottom: 1px solid black">Titulo</div>
-  <div>Corpo</div>
   </div>
-     <div class="card" >
-  <div style="border-bottom: 1px solid black">Titulo</div>
-  <div>Corpo</div>
-  </div>
-</div>
-</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import Card from '@/components/Card.vue';
+
 export default defineComponent({
   components: {
+    Card,
   },
 });
 </script>
 <style scoped>
-.card{
-border:1px solid black;
-margin:5px;
-padding:5px;
-width:200px;
-height:200px
-}
-
 </style>
